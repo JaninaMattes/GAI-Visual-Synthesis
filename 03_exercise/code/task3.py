2e1991dbd3139eefd1f8717fb379d0bc91673627
Janina Mattes <janina.alica.mattes@gmail.com>
2024-05-09 20:19:37 +0200

    src: Implementation of simple multi-layer MLP

--- 03_exercise/code/task3.py
+++ 03_exercise/code/task3.py
@@ -1,9 +1,13 @@
-from torchvision import datasets
 import torch
 import torch.nn as nn
-import numpy as np
 from torch.utils.data import DataLoader
+from torchvision import datasets
+import torchvision.transforms as transforms
+
 from tqdm.auto import tqdm
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 # The first thing to do is specify our model. The multilayer perceptron does a matrix
 # multiplication of its internal weights with the inputs and adds a bias in
@@ -21,23 +25,49 @@ from tqdm.auto import tqdm
 
 
 class MultilayerPerceptron(nn.Module):
+    """ The multilayer perceptron does a matrix multiplication of its 
+        internal weights with the inputs and adds a bias in
+        each layer. After that it activates the resulting vector.
+        This can be done using the `Linear` layer. 
+    """
     
     def __init__(self, size_hidden=100, size_out=10):
+        """ Each hidden layer has 100 hidden units (output feature dimension).
+            All hidden layers use ReLU activations.
+        """
         super().__init__()
+        # number of hidden nodes in each layer (100)
+        # linear layer (784 -> hidden_1)
+        self.fc1 = torch.nn.Linear(28*28, size_hidden) # Your Code here
+        # linear layer (n_hidden -> n_hidden)
+        self.fc2 = torch.nn.Linear(100, size_hidden)
+        # linear layer (n_hidden -> n_hidden)
+        self.fc3 = torch.nn.Linear(100, size_hidden)
+        # linear layer (n_hidden -> n_hidden)
+        self.fc4 = torch.nn.Linear(100, size_hidden)
+        # linear layer (n_hidden -> size_out)
+        self.out_layer = torch.nn.Linear(100, size_out)
         
-        self.fc1 = # Your Code here
-        self.fc2 = # Your Code here
-        self.fc3 = # Your Code here
-        self.fc4 = # Your Code here
-        self.out_layer = # Your Code here
-        
-        self.relu =# Your Code here
+        self.relu = torch.nn.ReLU()
         
     def forward(self, x):
+        # flatten the image, 
+        # the -1 is a wildcard
+        x = x.view(-1, 28*28)
         out = self.fc1(x)
         out = self.relu(out)
-
-        # Your Code here: The rest of the layers
+        out = self.fc2(out)
+        out = self.relu(out)
+        out = self.fc3(out)
+        out = self.relu(out)
+        out = self.fc4(out)
+        out = self.relu(out)
+        # flatten the output signal
+        out = out.view(out.size(0), -1)
+        out = self.out_layer(out)
+        
+        # no softmax as multi-class 
+        # classification not probabilities
 
         return out
 
@@ -50,29 +80,44 @@ class MultilayerPerceptron(nn.Module):
 # do an operation on those vectors you can find a `torch.Dataset` version of
 # the mnist vectors below. All it does is a simple casting operation.
 
+def convert_mnist_to_vectors(data):
+    '''Converts the ``[28, 28]`` MNIST images to vectors of size ``[28*28]``.
+       It outputs mnist_vectors as a array with the shape of [N, 784], where
+       N is the number of images in data.
+    '''
+
+    mnist_vectors = []
+    labels = []
+
+    for image, label in tqdm(data):
+        img_array = np.asarray(image)
+        img_array = np.reshape(img_array, -1) # reshape array to vector
+        mnist_vectors.append(img_array)
+        labels.append(label)
+
+    # return as numpy arrays
+    return np.array(mnist_vectors), labels
+
 
 class MnistVectors(torch.utils.data.Dataset):
-    '''A Pytorch Dataset, which does the same data preparation as was done in
-    the PCA exercise.'''
+    ''' A Pytorch Dataset, which does the same 
+        data preparation as was done in the PCA exercise.
+    '''
 
     def __init__(self, split='train'):
         super().__init__()
-        
-        mnist = datasets.MNIST('../data',
+        # torch dataset with casting operation
+        mnist_train = datasets.MNIST('../data',
                                train=split=='train',
                                download=True)
 
-        ########################
-        #### Your Code here ####
-        # self.mnist_vectors, self.labels = your conversion function from task 2
-        ########################
+        self.mnist_vectors, self.labels = convert_mnist_to_vectors(mnist_train)
 
             
     def __getitem__(self, idx):
-        '''Implements the ``[idx]`` method. Here we convert the numpy data to
-        torch tensors.
+        ''' Implements the ``[idx]`` method. Here we convert the numpy data to
+            torch tensors.
         '''
-
         mvec = torch.tensor(self.mnist_vectors[idx]).float()
         label = torch.tensor(self.labels[idx]).long()
 
@@ -80,43 +125,143 @@ class MnistVectors(torch.utils.data.Dataset):
     
     def __len__(self):
         return len(self.labels)
-
+    
 
 # The following two functions are needed to track the progress of the training.
 # One transforms the output of the MultilayerPerceptron into a scalar class label, the 
 # other uses that label to calculate the batch accuracy.
 
 def batch_accuracy(prediction, label):
-    return # Your Code here
+    """Uses labels to calculate the batch accuracy"""
+    N = prediction.size(0)
+    acc = prediction.eq(label.view_as(label)).sum().item()
+    return (acc / N) * 100
+
 
 def class_label(prediction):
-    return # Your code here
+    """ Transforms the output of the MultilayerPerceptron 
+        into a scalar class label 
+    """
+    return prediction.argmax(dim=1)
+
+def get_feature_maps(model, dataset=None, use_gpu=False):
+
+    # Visualize feature maps
+    act_fc1, act_fc2, act_fc3, act_fc4 = [], [], [], []
+    labels = []
+
+    # define hook
+    activation = {}
+
+    def get_activation(name):
+        def hook(model, input, output):
+            activation[name] = output.detach()
+        return hook
+
+    # register hooks
+    model.fc1.register_forward_hook(get_activation('fc1'))
+    model.fc2.register_forward_hook(get_activation('fc2'))
+    model.fc3.register_forward_hook(get_activation('fc3'))
+    model.fc4.register_forward_hook(get_activation('fc4'))
+
+    if not dataset:
+        dataset = datasets.MNIST(
+        root='../data',
+        train=False,
+        transform=transforms.ToTensor()
+    )
+
+    count = 0
+    with torch.no_grad():
+        for data_sample, label in dataset:
+            
+            data_sample = np.reshape(data_sample, -1)
+            data_sample.unsqueeze_(0)
+
+            if use_gpu:
+                data_sample = data_sample.cuda()
+                label = label.cuda()
+                
+            pred = model(data_sample)
+
+            act1 = activation['fc1'].squeeze()
+            act2 = activation['fc2'].squeeze()
+            act3 = activation['fc3'].squeeze()
+            act4 = activation['fc4'].squeeze()
+
+            # collect activated feature maps
+            act_fc1.append(act1.detach().cpu().numpy())
+            act_fc2.append(act2.detach().cpu().numpy())
+            act_fc3.append(act3.detach().cpu().numpy())
+            act_fc4.append(act4.detach().cpu().numpy())
+
+            # collect all labels
+            labels.append(label)
+            count += 1
+
+        print(f" Done after {count} steps!")
+    
+    return act_fc1, act_fc2, act_fc3, act_fc4, labels
+
+def validate(test, model, criterion, use_gpu=False):
+        test_accuracies = []
+        test_losses = []
+        
+        model.eval()
+        
+        with torch.no_grad():
+            for idx, [test_examples, test_labels] in enumerate(tqdm(test, desc='Test')):
+                if use_gpu:
+                    test_examples = test_examples.cuda()
+                    test_labels = test_labels.cuda()
+                
+                test_prediction = model(test_examples)
+                
+                # evaluation
+                test_loss = criterion(test_prediction, test_labels).data.item()
+                test_acc = batch_accuracy(class_label(test_prediction), test_labels)
+
+                if (idx % 10) == 0:
+                    print('Test Batch Accuracy: {}%, Test Loss: {}'.format(test_acc, test_loss))
+                
+                test_accuracies.append(test_acc) # per batch
+                test_losses.append(test_loss)
+
+        # Use the batch_accuracy function, also print the mean accuracy
+        # over the whole test split of the data.
+
+        print('Mean Validation Accuracy: {}%'.format(np.mean(test_accuracies)))
+        print('Mean Validation Loss: {}%'.format(np.mean(test_losses)))
+
+        plt.plot(test_accuracies)
 
 def train(use_gpu=False): # if torch.cuda.is_available(), use gpu to speed up training
     
     # Here we instantiate our model. The weights of the model are automatically
     # initialized by pytorch
-    P = MultilayerPerceptron()
+    model = MultilayerPerceptron()
     
     TrainData = MnistVectors()
     TestData = MnistVectors('test')
+
     # Dataloaders allow us to load the data in batches. This allows us a better
     # estimate of the parameter updates when doing backprop.
     # We need two Dataloaders so that we can train on the train data split
     # and evaluate on the test datasplit.
+    
     Dl = DataLoader(TrainData, batch_size=16, shuffle=True)
     testDl = DataLoader(TestData, batch_size=16, shuffle=False)
     
     # Use the Adam optimizer with learning rate 1e-4 and otherwise default
     # values
-    optimizer =  # Your code here
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
     # Use the Cross Entropy loss from pytorch. Make sure your MultilayerPerceptron does
-    # not use any activation function on the output layer! (Do you know why?)
-    criterion =  # Your code here
+    # not use any activation function on the output layer! (Do you know why?) 
+    criterion = nn.CrossEntropyLoss()
     
     if use_gpu:
-        P.cuda()
+        model.cuda()
         criterion.cuda()
     
     for epoch in tqdm(range(5), desc='Epoch'):
@@ -130,58 +275,42 @@ def train(use_gpu=False): # if torch.cuda.is_available(), use gpu to speed up tr
             # computed gradients are added on top of the existing ones. Thus
             # at before calculating new gradients we need to clear the old ones
             # using ther zero_grad() method.
+            
             optimizer.zero_grad()
             
-            prediction = P(example)
-            
+            prediction = model(example)
             loss = criterion(prediction, label)
             
             # Here pytorch applies backpropagation for us completely
-            # automatically!!! That is quite awesome!
             loss.backward()
 
             # The step method now adds the gradients onto the model parameters
             # as specified by the optimizer and the learning rate.
             optimizer.step()
             
-            # To keep track of what is happening print some outputs from time
-            # to time.
+            # To keep track of what is happening print some outputs from time to time.
             if (step % 375) == 0:
-                # Your code here
                 acc = batch_accuracy(class_label(prediction), label)
                 tqdm.write('Batch Accuracy: {}%, Loss: {}'.format(acc, loss))
-                
-        # Now validate on the whole test set
-        accuracies = []
-        for idx, [test_ex, test_l] in enumerate(tqdm(testDl, desc='Test')):
-            if use_gpu:
-                test_ex = test_ex.cuda()
-                test_l = test_l.cuda()
-
-            #########################
-            #### Your Code here  ####
-            #########################
-
-            # Using your batch_accuracy function, also print the mean accuracy
-            # over the whole test split of the data.
-
-        print('Validation Accuracy: {}%'.format(np.mean(accuracies)))
-
-        # Now let's write out a checkpoint of the model, so that we can
-        # reuse it:
+        
+        # checkpoints to reuse model weights
         torch.save(model.state_dict(), 'perceptron_{}.ckpt'.format(step))
 
-        # If you need to load the checkpoint instanciate your model and the
-        # load the state dict from a checkpoint:
-        # P = MultilayerPerceptron()
-        # P.load_state_dict(torch.load(perceptron_3750.ckpt))
-        # Make sure to use the latest checkpoint by entering the right number.
+    # Now validate on the whole test set
+    validate(testDl, model, criterion, use_gpu)
 
-        ######################################
-        ###### Add code for task 4 here ######
-        ######################################
+    # If you need to load the checkpoint instanciate your model and the
+    # load the state dict from a checkpoint:
+    # model = MultilayerPerceptron()
+    # model.load_state_dict(torch.load(perceptron_3750.ckpt))
+    # Make sure to use the latest checkpoint by entering the right number.
 
-        
+    ######################################
+    ######      Code for task 4     ######
+    ######################################
+
+    get_feature_maps(model, use_gpu=use_gpu)
+
 
 
 if __name__ == '__main__':
